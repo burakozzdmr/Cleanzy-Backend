97b224f2e509a79e810da89b24959203480ce97a
Burak Özdemir <mburak.ozdemir03@gmail.com>
2026-03-31 18:11:39 +0300

    Add: base package structure for auth module

---
+++ src/main/java/com/burakozdemir/cleanzy/CleanzyApplication.java
@@ -0,0 +1,13 @@
+package com.burakozdemir.cleanzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanzyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burakozdemir/cleanzy/CleanzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakozdemir.cleanzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanzyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
