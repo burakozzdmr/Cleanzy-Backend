d43d8e418722c3d3337562731734e6d0603118db
Burak Özdemir <mburak.ozdemir03@gmail.com>
2026-04-06 01:20:47 +0300

    Edit: refactoring in the repository for service layer

--- src/main/java/com/burakozdemir/cleanzy/customer/repository/CustomerRepository.java
+++ src/main/java/com/burakozdemir/cleanzy/customer/repository/CustomerRepository.java
@@ -1,6 +1,11 @@
 package com.burakozdemir.cleanzy.customer.repository;
 
+import com.burakozdemir.cleanzy.auth.entity.User;
 import com.burakozdemir.cleanzy.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> { }
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByUser(User user);
+}
